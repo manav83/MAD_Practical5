26dce2ad288dcc3622f7fd8ded8c7aac3d3d3839
Manav Sadavrati <110801341+manav83@users.noreply.github.com>
2022-11-04 02:16:10 +0530

    Add files via upload

---
+++ Practical 5/app/src/main/java/com/example/madpractical5_21012022050/MyService.kt
@@ -0,0 +1,52 @@
+package com.example.madpractical5_21012022050
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+import com.example.madpractical5_21012022050.R
+
+class MyService : Service() {
+
+    companion object{
+        var DATA_KEY="service"
+        const val DATA_VALUE="play/pause"
+
+    }
+    private lateinit var player: MediaPlayer
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (!this::player.isInitialized) {
+            player = MediaPlayer.create(this, R.raw.song11
+            )
+        }
+        if (intent != null)
+        {
+            val str=intent.getStringExtra(DATA_KEY)
+            if(str== DATA_VALUE)
+            {
+                if(player.isPlaying)
+                {
+                    player.pause()
+                }
+                else{
+                    player.start()
+                }
+            }
+        }
+        else{
+            player.start()
+        }
+
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        player.stop()
+        super.onDestroy()
+    }
+
+}
